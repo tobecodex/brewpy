f74e045237949b74880e811067d707faa7b27f32
root <root@raspberrypi.(none)>
2014-02-03 01:08:53 +0000

    Swap to thermistor temp measurements

--- brewpy/monitor.py
+++ brewpy/monitor.py
@@ -2,8 +2,35 @@ import time
 import datetime
 import subprocess
 import RPi.GPIO as GPIO
+from operator import itemgetter
 
 __PIN_HEATER = 23
+__PIN_THERMISTOR = 23
+
+def read_thermistor():
+  curve = []
+  values = open("curve.csv").readlines()
+  for value in values:
+    t, r = value.split(",")
+    curve.append((int(r.strip()), int(t)))
+  curve.sort(key = itemgetter(0))
+
+  stdout = subprocess.check_output(
+    ["/usr/local/sbin/analog", __PIN_THERMISTOR]
+  ).strip()
+  r = int(stdout)
+
+  i = 0
+  while i < len(curve) and r > curve[i][0]:
+    i += 1
+
+  if i == 0 or i >= len(curve):
+    return None
+  
+  d = curve[i][0] - r
+  dT = curve[i][0] - curve[i - 1][0]
+  t = curve[i][1] + (1 * (d / float(dT)))
+  return t
 
 def run():
   # Setup
